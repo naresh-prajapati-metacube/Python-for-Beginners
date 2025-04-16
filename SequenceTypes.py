b31b85e24e1596ec36c75232c1282d8cabe01e6e
naresh.prajapati <naresh.prajapati@metacube.com>
2025-04-16 16:30:47 +0530

    added code

---
+++ SequenceTypes.py
@@ -0,0 +1,14 @@
+countries = ['India', 'United States', 'Russia']
+print(countries)
+
+countries.append('Australia')
+
+print(countries)
+
+del countries[1]
+
+print(countries)
+
+countries.insert(2, 'Mexico')
+
+print(countries)
